743a8382ed929efab9125562d3f667cf81ee807f
Tim Etchells <timetchells@ibm.com>
2018-10-12 12:55:56 -0400

    further travis improvements

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Microclimate Developer Tools for VSCode
 
 [![Build Status](https://travis.ibm.com/dev-ex/microclimate-vscode.svg?token=jLZpzPrJozeLHsb1tpsR&branch=master)](https://travis.ibm.com/dev-ex/microclimate-vscode)
+
+For the README packaged with this extension, see README.vscode.md
---
+++ src/command/ToggleEnablementCmd.ts
@@ -0,0 +1,26 @@
+import * as vscode from "vscode";
+
+import Project from "microclimate/project/Project";
+import { promptForProject } from "command/CommandUtil";
+
+export default async function requestBuildCmd(project: Project): Promise<void> {
+    console.log("RequestBuildCmd invoked");
+    if (project == null) {
+        const selected = await promptForProject();
+        if (selected == null) {
+            // user cancelled
+            console.log("User cancelled project prompt");
+            return;
+        }
+        project = selected;
+    }
+
+    if (project.state.isBuilding) {
+        vscode.window.showErrorMessage(`${project.name} is already building`);
+        return;
+    }
+
+    console.log(`Request build for project ${project.name}`);
+
+    project.connection.requestBuild(project);
+}
