d855b70a19ef78d2f2c2f94fedf7261f860d561e
Tim Etchells <timetchells@ibm.com>
2018-10-23 11:45:29 -0400

    Add calling file and function to logger, needs testing.

--- dev/src/Logger.ts
+++ dev/src/Logger.ts
@@ -58,8 +58,6 @@ export class Logger {
             return;
         }
 
-        // const stack = new Error().stack;
-
         const argsStr: string = args.reduce( (result: string, arg: any): string => {
             if (arg instanceof Object) {
                 try {
@@ -76,7 +74,15 @@ export class Logger {
             return result;
         }, s);
 
-        const label = `[${level}: ${this.getDateTime()}]:`;
+        let caller = "";
+        try {
+            caller = " " + getCaller();
+        }
+        catch(err) {
+            console.error(err);
+        }
+
+        const label = `[${level}: ${getDateTime()}${caller}]:`;
         const msg: string = `${label} ${argsStr}${os.EOL}`;
 
         return new Promise<void>( (resolve, _) => {
@@ -97,16 +103,56 @@ export class Logger {
             });
         });
     }
-
-    private static getDateTime(): string {
-        const now = new Date();
-        // formats to eg. 22/10/2018 9:40:15
-        // Note add 1 to month because months are 0-indexed
-        return `${leftPad(now.getDate())}/${leftPad(now.getMonth() + 1)}/${now.getFullYear()} ` +
-            `${leftPad(now.getHours())}:${leftPad(now.getMinutes())}:${leftPad(now.getSeconds())}`;
-    }
 }
 
+const callingFileRegex: RegExp  = /\w+\.js/g;
+// const callingFnRegex: string    = `at\s(\w)+\s`;
+
+function getCaller(): string {
+    const stack = new Error().stack;
+    if (stack != null) {
+        const stackLines = stack.split("\n");
+        console.log("stackLines:", stackLines);
+        for (const [i, line] of stackLines.reverse().entries()) {
+            if (line.includes(__filename)) {
+                const callerRaw: string = stackLines[i-1].trim();
+                // the callerRaw line will look like this:
+                // "at activate (/Users/tim/programs/microclimate-vscode/dev/out/extension.js:13:21)"
+
+                const splitResult: string[] = callerRaw.split(" ");
+                let callerFn = "";
+                if (splitResult.length > 1) {
+                    callerFn = splitResult[1];
+                }
+                else {
+                    console.log("Couldn't parse function name from:", callerRaw);
+                    return "";
+                }
+                // filepath will be like "(/Users/tim/programs/microclimate-vscode/dev/out/extension.js:13:21)"
+                const filepath = splitResult[splitResult.length - 1];
+
+                let callerFile = "";
+                const filenameMatches: RegExpMatchArray | null = filepath.match(callingFileRegex);
+                if (filenameMatches != null && filenameMatches.length > 0) {
+                    callerFile = filenameMatches[0];
+                }
+                console.log(`callerFn "${callerFn}" callerFile "${callerFile}"`);
+
+                return `${callerFile}.${callerFn}()`;
+            }
+        }
+    }
+    console.error("Couldn't find caller line");
+    return "";
+}
+
+function getDateTime(): string {
+    const now = new Date();
+    // formats to eg. 22/10/2018 9:40:15
+    // Note add 1 to month because months are 0-indexed
+    // return `${leftPad(now.getDate())}/${leftPad(now.getMonth() + 1)}/${now.getFullYear()} ` +
+    return `${leftPad(now.getHours())}:${leftPad(now.getMinutes())}:${leftPad(now.getSeconds())}.${now.getMilliseconds()}`;
+}
 // converts a 1-char number to a 2-char with a leading 0. eg. leftPad("2") -> "02"
 function leftPad(n: number): string {
     if (n >= 10) {
