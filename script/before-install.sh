b3dddfc53a25f0d7fb6fd919584f97a2f5b2053d
Tim Etchells <timetchells@ibm.com>
2019-01-14 12:21:39 -0500

    add copyright to scripts

--- script/before-install.sh
+++ script/before-install.sh
@@ -1,4 +1,16 @@
 #!/usr/bin/env bash
+
+#*******************************************************************************
+# Copyright (c) 2018, 2019 IBM Corporation and others.
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Eclipse Public License v2.0
+# which accompanies this distribution, and is available at
+# http://www.eclipse.org/legal/epl-v20.html
+#
+# Contributors:
+#     IBM Corporation - initial API and implementation
+#*******************************************************************************
+
 # WD should be dev/
 
 set -ex
