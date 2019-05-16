648e27503f1c7528621a20f49070dc130f149b7f
Tim Etchells <timetchells@ibm.com>
2019-05-16 10:07:20 -0400

    Removed unused container-logs event

--- dev/src/microclimate/connection/SocketEvents.ts
+++ dev/src/microclimate/connection/SocketEvents.ts
@@ -85,8 +85,6 @@ namespace SocketEvents {
         PROJECT_DELETION = "projectDeletion",
         PROJECT_RESTART_RESULT = "projectRestartResult",
         PROJECT_SETTING_CHANGED = "projectSettingsChanged",
-        // deprecated, replaced by log-update
-        CONTAINER_LOGS = "container-logs",
         LOG_UPDATE = "log-update",
         LOGS_LIST_CHANGED = "projectLogsListChanged",
         PROJECT_VALIDATED = "projectValidated",
